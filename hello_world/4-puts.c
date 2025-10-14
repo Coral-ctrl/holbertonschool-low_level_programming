9eab114e8a2a5611b700220a14db9507947e815a
Shan <liangyongshan33@gmail.com>
2025-10-14 03:54:29 +0000

    add 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point
- * puts -  Writes the string s and a trailing newline to stdout
+ * puts - writes the string s and a trailing newline to stdout
  *
  * Return: 0 on success
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle");
-    return (0);
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
