e29806ed507f94f2d5cf18d1072e1a908b316e26
Shan <liangyongshan33@gmail.com>
2025-10-14 03:37:16 +0000

    add 4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * puts -  Writes the string s and a trailing newline to stdout
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
+}
