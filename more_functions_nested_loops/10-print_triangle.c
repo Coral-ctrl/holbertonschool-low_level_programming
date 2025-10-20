25ca3bc8d9461c4bfc8371f831fdcb1a68c5ce24
Shan <liangyongshan33@gmail.com>
2025-10-20 01:49:35 +0000

    add 10-print_triangle.c main.h

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_triangle - prints a triangle
+ * @size: integer to check
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 0; j < size - i; j++)
+				_putchar(' ');
+			for (j = 0; j < i; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
