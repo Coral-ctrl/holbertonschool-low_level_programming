15f459effa447cf802fda7c0236b21028b00f23b
Shan <liangyongshan33@gmail.com>
2025-10-21 00:13:10 +0000

    add 4-print_rev.c main.h

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to print
+ */
+void print_rev(char *s)
+{
+	int i = 0, j;
+
+	while (s[i] != '\0')
+		i++;
+	j = i - 1;
+
+	while (j >= 0)
+	{
+		_putchar(s[j]);
+		j--;
+	}
+	_putchar('\n');
+}
