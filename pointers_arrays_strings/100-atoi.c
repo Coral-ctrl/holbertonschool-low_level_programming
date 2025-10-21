54e11d62bb5992370047a8fd524549d090da7c67
Shan <liangyongshan33@gmail.com>
2025-10-21 02:12:02 +0000

    add 100-atoi.c

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+/**
+ * _atoi - convert a string to an integer
+ * @s: string to convert
+ *
+ * Return: integer value of the string
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int sign = 1;
+	int found_digit = 0;
+	int result = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			found_digit = 1;
+			while (s[i] >= '0' && s[i] <= '9')
+			{
+				result = (s[i] - '0') + result * 10;
+				i++;
+			}
+		}
+		i++;
+	}
+
+	if (found_digit == 0)
+		return (0);
+	else
+		return (result * sign);
+}
