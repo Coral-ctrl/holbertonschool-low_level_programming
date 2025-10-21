49b727228f3b4a27f8bef297e10cb3c3abf1b4c7
Shan <liangyongshan33@gmail.com>
2025-10-21 00:04:08 +0000

    add 3-puts.c main.h

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,9 +1,15 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * _puts - prints a string to stdout
  * @str: string to print
  */
 void _puts(char *str)
 {
-	fputs(str, stdout);
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
