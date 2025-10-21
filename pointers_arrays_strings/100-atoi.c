03bb7e9271a0331bafaf924f893d962cc79447ae
Shan <liangyongshan33@gmail.com>
2025-10-21 02:29:17 +0000

    modify 10 again

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -18,18 +18,22 @@ int _atoi(char *s)
 		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			found_digit = 1;
-			while (s[i] >= '0' && s[i] <= '9')
-			{
-				result = (s[i] - '0') + result * 10;
-				i++;
-			}
 			break;
 		}
 		i++;
 	}
 
+	while (s[i] >= '0' && s[i] <= '9')
+	{
+		if (sign == 1)
+			result = (s[i] - '0') + result * 10;
+		else
+			result = result * 10 - (s[i] - '0');
+		i++;
+	}
+
 	if (found_digit == 0)
 		return (0);
-	else
-		return (result * sign);
+
+	return (result);
 }
