8d8f3fa6d47e0b5af1b5e646c0fee492fac80109
Shan <liangyongshan33@gmail.com>
2025-10-28 04:12:37 +0000

    add 6-is_prime_number.c

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+/**
+ * check_prime - check whether it is prime
+ * @n: number to check
+ * @i: current number being tested
+ *
+ * Return: whether it is prime
+ */
+int check_prime(int n, int i)
+{
+	if (i * i > n)
+		return (1);
+
+	if (n % i == 0)
+		return (0);
+
+	return (check_prime(n, i+1));
+}
+
+/**
+ * is_prime_number - returns 1 if the input integer is a prime number
+ * @n: number to check
+ *
+ * Return: 1 if prime, 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (check_prime(n, 2));
+}
