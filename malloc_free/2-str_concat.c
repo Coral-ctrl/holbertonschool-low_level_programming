5604db3baf6591399dbde3326917504f74396bf9
“Shan” <liangyongshan33@gmail.com>
2025-11-03 01:03:49 +0000

    modify 2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,9 +11,11 @@ char *str_concat(char *s1, char *s2)
 	char *con;
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-
+	if (s1 == NULL)
+		s1[len1] = '\0';
+	if (s2 == NULL)
+		s2[len2] = '\0';
+	
 	while (s1[len1] != '\0')
 		len1++;
 
