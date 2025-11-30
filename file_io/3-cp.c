197c16be3bed55be8bc85b269f122e48e1b271f2
Shan <liangyongshan33@gmail.com>
2025-11-30 01:07:45 +0000

    Add 3-cp.c

---
+++ file_io/3-cp.c
@@ -0,0 +1,83 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * close_fd - closes a file descriptor and handles error
+ * @fd: file descriptor
+ */
+void close_fd(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - copies content from one file to another
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int fd_from, fd_to;
+	ssize_t r, w;
+	char buffer[BUFFER_SIZE];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", argv[1]);
+                exit(98);
+	}
+
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO,
+                        "Error: Can't write to %s\n", argv[2]);
+		close_fd(fd_from);
+                exit(99);
+	}
+
+	while ((r = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		w = write(fd_to, buffer, r);
+		if (w == -1 || w != r)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to file %s\n", argv[2]);
+			close_fd(fd_from);
+			close_fd(fd_to);
+			exit(99);
+		}
+	}
+
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO,
+                        "Error: Can't read from file %s\n", argv[1]);
+		close_fd(fd_from);
+		close_fd(fd_to);
+                exit(98);
+	}
+
+	close_fd(fd_from);
+	close_fd(fd_to);
+
+	return (0);
+}
