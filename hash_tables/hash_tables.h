34e46ecf78a4e9e95983a42e9fd21468df74d350
“Shan” <liangyongshan33@gmail.com>
2025-12-01 23:22:12 +0000

    Update hash_tables.h

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -39,5 +39,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 hash_node_t *create_node(const char *key, const char *value);
 hash_node_t *find_node(hash_node_t *head, const char *key);
 int update_node(hash_node_t *node, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 
 #endif
