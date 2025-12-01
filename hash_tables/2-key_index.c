d20b6416db670a8b91e7e401864acac930bbc518
“Shan” <liangyongshan33@gmail.com>
2025-12-01 10:50:29 +0000

    Add hash_tables.h 2-key_index.c

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+#include "hash_tables.h"
+/**
+ * key_index - gives you the index of a key
+ * @key: the key
+ * @size: the size of the array of the hash table
+ *
+ * Return: the index at which the key/value pair should be stored in the
+ *         array of the hash table
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	/* Hash the key using hash_djb2()
+	 * Use modulo (% size) to map the huge hash number into array range
+	 * Return that index */
+	return (hash_djb2(key) % size);
+}
