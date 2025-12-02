f4bbd43b70418f4a5aba06caae5a6995989a50f7
“Shan” <liangyongshan33@gmail.com>
2025-12-02 00:07:13 +0000

    Update: hash_tables.h Add: 6-hash_table_delete.c

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -41,5 +41,6 @@ hash_node_t *find_node(hash_node_t *head, const char *key);
 int update_node(hash_node_t *node, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
 
 #endif
